4b2b94512dd25aba1171a2e585d4cb59e09fc6a8
mistr <michal.stransky42@gmail.com>
2025-06-19 22:44:12 +0200

    add comments

--- main.rs
+++ main.rs
@@ -1,29 +1,33 @@
 use std::io::stdin;
 enum Operation {
     Addition,
-    Substraction,
+    Subtraction,
     Division,
     Multiplication,
 }
 struct Number {
     operation: Option<Operation>,
+    value: i32,
 }
 fn main() {
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
 }
-fn input_parse(str: &String, numbers: &mut Vec<Number>) {
-    let num_index = 0;
-    for (i, c) in str.chars().enumerate() {
-        let mut num: Number;
-        num.operation = Some(match c {
+fn input_parse(input: &str) {
+    let mut numbers: Vec<Number> = Vec::new();
+    let mut num_index = 0;
+    for (i, c) in input.chars().enumerate() {
+        let op = Some(match c {
             '+' => Operation::Addition,
-            '-' => Operation::Substraction,
+            '-' => Operation::Subtraction,
             '*' => Operation::Multiplication,
             '/' => Operation::Division,
             _ => {
                 continue;
             }
         });
+
+        // first argument has no operation
     }
+    // add last argument
 }
